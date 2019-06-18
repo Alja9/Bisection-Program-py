ed9233a12fb78eb42e4a2a80b4d53b3a22c9f135
Alja9 <34116297+Alja9@users.noreply.github.com>
2019-06-19 00:36:19 +0700

    Create biseksiprogram.py

---
+++ biseksiprogram.py
@@ -0,0 +1,20 @@
+iterasi = 0
+a = 0.0
+b = 1.0
+def pers(x):
+       return x**3 - 2*(x**2) + 6*x - 4.0
+print("Iterasi\t\ta\t\tb\t\tNilai Tengah")
+print("-"*60)
+while((b-a) >= 0.01):
+       c = (a+b)/2
+       print("%d\t\t%f\t%f\t%f" % (iterasi, a, b, c))
+       if(pers(c)==0.0):
+              break
+       if(pers(c)*pers(a)<0):
+              b = c
+       else:
+              a = c
+       iterasi+=1
+print("DONE")
+              
+              
